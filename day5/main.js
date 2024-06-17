30bc82ed57924ee37ea36af7f4482a6affee6f17
Ayush Jangid <94820278+ayushjangid1@users.noreply.github.com>
2024-06-17 11:42:36 +0530

    Add files via upload

---
+++ day5/main.js
@@ -0,0 +1,29 @@
+// 1. arrow functions
+
+// const sum = (a,b) => {
+//     return a+b;
+// }
+
+// console.log(sum(5,5));
+
+// 2. Map Runns transformation on array
+
+// const INP = [1,2,3,4,5];
+
+// function transform(i){
+//     return 2*i;
+// }
+
+// const ans = INP.map(transform);
+// console.log(ans);
+
+// 3. Filter
+
+// const INP = [1,2,3,4,5];
+
+// const ans = INP.filter((n)=>{
+//     if(n%2==0)return true;
+//     else return false;
+// });
+
+// console.log(ans);
